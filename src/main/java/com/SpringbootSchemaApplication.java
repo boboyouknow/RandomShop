1c9bf05cdfe3bc6b3ea4095d26cc31e20d24ad87
“moke” <xieyl321@163.com>
2023-04-02 01:37:24 +0800

    无事发生

--- src/main/java/com/SpringbootSchemaApplication.java
+++ src/main/java/com/SpringbootSchemaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+//启动类
 @SpringBootApplication
 @MapperScan(basePackages = {"com.dao"})
 public class SpringbootSchemaApplication extends SpringBootServletInitializer{
